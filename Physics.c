258daed2f7f635dfcf4d3181fe035861e731d7f6
Ruan Montelo <ruanmontelo@gmail.com>
2023-04-21 13:56:50 -0300

    Fixed cube collisions so that there are no spaces between the cubes when computing the collision

--- Physics.c
+++ Physics.c
@@ -16,9 +16,9 @@ int collision_AABB(Cube cube1, Cube cube2) {
 	cube2.cube_faces.left = cube2.pos.x - cube2.width / 2;
 	cube2.cube_faces.right = cube2.pos.x + cube2.width / 2;
 
-	int x_collision = cube1.cube_faces.right >= cube2.cube_faces.left && cube1.cube_faces.left <= cube2.cube_faces.right;
-	int y_collision = cube1.cube_faces.top >= cube2.cube_faces.bottom && cube1.cube_faces.bottom <= cube2.cube_faces.top;
-	int z_collision = cube1.cube_faces.front >= cube2.cube_faces.back && cube1.cube_faces.back <= cube2.cube_faces.front;
+	int x_collision = cube1.cube_faces.right > cube2.cube_faces.left && cube1.cube_faces.left < cube2.cube_faces.right;
+	int y_collision = cube1.cube_faces.top > cube2.cube_faces.bottom && cube1.cube_faces.bottom < cube2.cube_faces.top;
+	int z_collision = cube1.cube_faces.front > cube2.cube_faces.back && cube1.cube_faces.back < cube2.cube_faces.front;
 
 	return x_collision & y_collision & z_collision;
 }
